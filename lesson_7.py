d6a4236168a533204fbbc0f701bd62ecc61135a6
Stanislav Ryndin <slava.jax@gmail.com>
2021-07-03 21:02:42 +0300

    Урок №7

---
+++ lesson_7.py
@@ -0,0 +1,24 @@
+# test = None
+# print(test)
+
+# # x, y = 1, 5
+# print(x, y)
+
+# bool
+my_true = True
+my_false = False
+
+print(f'my_true - {my_true}, type - {type(my_true)}')
+print(f'my_false - {my_false}, type - {type(my_false)}')
+
+# str() int()  float() bool()
+x = 5.6
+print(x, type(x))
+x = str(x)
+print(x, type(x))
+
+x = bool(x)
+print(x, type(x))
+
+print(bool(0))
+print(bool(''))
