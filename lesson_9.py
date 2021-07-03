4482faa3cde88b9812f845a190789ce28a5d3881
Stanislav Ryndin <slava.jax@gmail.com>
2021-07-03 21:43:43 +0300

    Урок №9

---
+++ lesson_9.py
@@ -0,0 +1,22 @@
+# Сырая строка / raw string
+s = r'C:\worker\new.txt'
+
+# s = 'Py' 'thon'
+# print(s)
+
+# s1 = 'Hello, '
+# s2 = 'city!'
+# s3 = s1 + s2
+# print(s3)
+
+# name = 'John'
+# age = 33
+# print('My name is ' + name + " I'm " + str(age))
+
+s = 'Hello, city!'
+print(s[-2:])  # y!
+print(s[3:-3])  # lo, ci
+print(s[::-1])  # !ytic ,olleH
+print(s[:5])  # Hello
+print(s[::2])  # Hlo iy
+print(s[:5] + s[7:])
